022fe8eaf47ff37fc8487c55b963ba126c4c23d3
williamellings <wille.ellings@hotmail.com>
2026-05-03 17:00:55 +0200

    Commands and handlers

---
+++ CleanWebAPI.Application/Products/Commands/CreateProductCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+
+namespace CleanWebAPI.Application.Products.Commands;
+
+public record CreateProductCommand(
+    string Name,
+    string Description,
+    decimal Price,
+    Guid CategoryId) : IRequest<Guid>;
---
+++ CleanWebAPI.Application/Products/Handlers/CreateProductHandler.cs
@@ -0,0 +1,33 @@
+﻿using CleanWebAPI.Application.Products.Commands;
+using CleanWebAPI.Domain.Entities;
+using CleanWebAPI.Domain.Interfaces;
+using MediatR;
+
+namespace CleanWebAPI.Application.Products.Handlers;
+
+public class CreateProductHandler : IRequestHandler<CreateProductCommand, Guid>
+{
+    private readonly IProductRepository _repository;
+
+    public CreateProductHandler(IProductRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Guid> Handle(CreateProductCommand request,CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name,
+            Description = request.Description,
+            Price = request.Price,
+            CategoryId = request.CategoryId
+        };
+
+        await _repository.AddAsync(product);
+        await _repository.SaveChangesAsync();
+
+        return product.Id;
+    }
+}
